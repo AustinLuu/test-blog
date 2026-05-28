94d4a3d09a803f657f9c351f5fd6ab3f3e1e72c9
Austin Luu <austinowenluu@gmail.com>
2026-05-28 01:08:02 -0400

    Stylistic changes and optimizations

--- pages/index.js
+++ pages/index.js
@@ -5,27 +5,26 @@ import About from '../components/About'
 import Projects from '../components/Projects'
 import Timeline from '../components/Timeline'
 import Footer from '../components/Footer'
-import Script from 'next/script'
+import ParticleCanvas from '../components/ParticleCanvas'
 
 const title = 'Portfolio – Austin Luu';
 
 export default function Home() {
   return (
-    
+
     <div className='App'>
       <Head>
         <title>{title}</title>
         <meta name="description" content="Austin Luu - Portfolio" />
         <link rel="icon" href="/static/images/logo.png" />
       </Head>
-      <div id="root"></div><canvas id="canvas"></canvas>
+      <ParticleCanvas/>
       <Navbar/>
       <Header/>
       <About/>
       <Projects/>
       <Timeline/>
       <Footer/>
-    <Script src="/static/script.js" strategy="lazyOnload" />
     </div>
   )
 }
